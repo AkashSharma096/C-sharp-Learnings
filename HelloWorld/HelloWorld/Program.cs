c21b026efd573738125f0b40fcbe75863e3d4eea
AkashSharma023 <127305987+AkashSharma023@users.noreply.github.com>
2025-07-12 19:50:49 +0530

    Commit helloworld

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,4 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("This is a simple console application.");
-Console.WriteLine("Press any key to exit...");
