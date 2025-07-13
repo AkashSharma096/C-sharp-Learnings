4ed59830c8b6f29cc5dd44aa0e604dd513d646cf
AkashSharma023 <127305987+AkashSharma023@users.noreply.github.com>
2025-07-13 20:39:32 +0530

    Adding inheritance project

---
+++ Inheritance/Inheritance/Program.cs
@@ -0,0 +1,44 @@
+﻿class Program
+{
+    static void Main()
+    {
+
+        /************* Creating object for Parent class ***************/
+        Person p = new Person();
+        p.personName = "Jane Doe";
+        p.personGender = "Male";
+
+        System.Console.WriteLine($"Person Name: {p.personName}");
+        System.Console.WriteLine($"Person Gender: {p.personGender}");
+
+        /************* Creating object for Child classes ***************/
+
+        // Creating object for Student class
+        Student s = new Student();
+        s.personName = "John Smith";
+        s.personGender = "Male";
+        s.studentId = "S12345";
+        s.studentGrade = "A";
+
+        System.Console.WriteLine($"Student Name: {s.personName}");
+        System.Console.WriteLine($"Student Gender : {s.personGender}");
+        System.Console.WriteLine($"Student ID: {s.studentId}");
+        System.Console.WriteLine($"Student Grade: {s.studentGrade}");
+
+
+        // Creating object for Employee class
+        Employee e = new Employee();
+        e.personName = "Alice Johnson";
+        e.personGender = "Female";
+        e.employeeId = "E67890";
+        e.employeeSalary = 75000.00;
+
+        System.Console.WriteLine($"Employee Name: {e.personName}");
+        System.Console.WriteLine($"Employee Gender: {e.personGender}");
+        System.Console.WriteLine($"Employee ID: {e.employeeId}");
+        System.Console.WriteLine($"Employee Salary: {e.employeeSalary}");
+
+        System.Console.ReadLine();
+
+    }
+}
---
+++ Inheritance/Person/Class1.cs
@@ -0,0 +1,18 @@
+﻿public class Person
+{
+    public string personName { get; set; }
+    public string personGender { get; set; }
+}
+
+
+public class Student : Person
+{
+    public string studentId { get; set; }
+    public string studentGrade { get; set; }
+}
+
+public class Employee : Person
+{
+    public string employeeId { get; set; }
+    public double employeeSalary { get; set; }
+}
