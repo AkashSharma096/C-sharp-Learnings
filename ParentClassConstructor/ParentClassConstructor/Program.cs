a9836323d34aa573628226f27eaefdbfd87cd27c
AkashSharma023 <127305987+AkashSharma023@users.noreply.github.com>
2025-07-14 12:20:07 +0530

    Adding Parent class constructor project

---
+++ ParentClassConstructor/ParentClassConstructor/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Student s = new Student("Allen", 20, "Male", "S12345", 85);
+        s.DisplayStudentInfo();
+        Console.WriteLine("\n");
+
+
+        Employee e = new Employee("Sarah", 25, "Female", 101, 50000);
+        e.DisplayEmployeeInfo();
+        Console.WriteLine("\n");
+    }
+}
