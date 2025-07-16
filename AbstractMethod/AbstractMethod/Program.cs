9b29eece847ecb5315e2b4e8fa48e62fca3399f7
AkashSharma023 <127305987+AkashSharma023@users.noreply.github.com>
2025-07-16 22:31:22 +0530

    Adding Abstract Method

---
+++ AbstractMethod/AbstractMethod/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Employee emp = new Employee
+        {
+            PersonName = "John Doe",
+            PersonGender = "Male",
+            EmployeeId = 12345,
+            EmployeeSalary = 75000.00
+        };
+
+        emp.Display();
+
+        Student stu = new Student
+        {
+            PersonName = "Jane Smith",
+            PersonGender = "Female",
+            StudentId = 54321,
+            StudentMarks = 88
+        };
+
+        stu.Display();
+
+    }
+}
---
+++ AbstractMethod/Person/Class1.cs
@@ -0,0 +1,38 @@
+﻿public abstract class Person
+{
+    public required string PersonName { get; set; }
+    public required string PersonGender { get; set; }
+
+    public abstract void Display();
+}
+
+
+public class Employee : Person
+{
+    public int EmployeeId { get; set; }
+    public double EmployeeSalary { get; set; }
+
+    public override void Display()
+    {
+        Console.WriteLine("Employee Name : " + base.PersonName);
+        Console.WriteLine("Employee Gender : " + base.PersonGender);
+        Console.WriteLine("Employee ID : " + this.EmployeeId);
+        Console.WriteLine("Employee Salary : " + this.EmployeeSalary);
+        Console.WriteLine("\n");
+    }
+}
+
+public class Student : Person
+{
+    public int StudentId { get; set; }
+    public int StudentMarks { get; set; }
+
+    public override void Display()
+    {
+        Console.WriteLine("Student Name : " + base.PersonName);
+        Console.WriteLine("Student Gender : " + base.PersonGender);
+        Console.WriteLine("Student ID : " + this.StudentId);
+        Console.WriteLine("Student Marks : " + this.StudentMarks);
+        Console.WriteLine("\n");
+    }
+}
