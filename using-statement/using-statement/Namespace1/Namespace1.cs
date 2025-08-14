1b7e837d6b1fc62deb64ddc44d8dd41ba405d165
AkashSharma023 <127305987+AkashSharma023@users.noreply.github.com>
2025-08-14 22:11:01 +0530

    Namespace with USING statement

---
+++ using-statement/using-statement/Namespace1/Namespace1.cs
@@ -0,0 +1,11 @@
+namespace Namespace1
+{
+    public class Class1
+    {
+        public void Method1()
+        {
+            // Implementation of Method1
+            System.Console.WriteLine("Namespace1 Class1 Method1 called");
+        }
+    }
+}
---
+++ using-statement/using-statement/Program.cs
@@ -0,0 +1,13 @@
+﻿using Namespace1;
+
+namespace NamespaceExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            Class1 class1 = new Class1();
+            class1.Method1();
+        }
+    }
+}
