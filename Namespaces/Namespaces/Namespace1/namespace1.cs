5c3a7e76d930541cf3b7b06b683d06003c3ff9a0
AkashSharma023 <127305987+AkashSharma023@users.noreply.github.com>
2025-08-14 21:55:13 +0530

    Adding NAmespace project

---
+++ Namespaces/Namespaces/Namespace1/namespace1.cs
@@ -0,0 +1,10 @@
+namespace Namespace1
+{
+    public class Class1
+    {
+        public void Method1()
+        {
+            System.Console.WriteLine("Namespace1.Class1 Method1");
+        }
+    }
+}
---
+++ Namespaces/Namespaces/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace NamespaceExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            Namespace1.Class1 class1 = new Namespace1.Class1();
+            class1.Method1();
+            System.Console.ReadKey();
+        }
+    }
+}
