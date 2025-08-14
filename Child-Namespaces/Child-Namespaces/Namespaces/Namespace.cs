a0790bf79f8ffcc0ab568254e0bad2390789a588
AkashSharma023 <127305987+AkashSharma023@users.noreply.github.com>
2025-08-14 22:03:17 +0530

    Adding Child Namespaces

---
+++ Child-Namespaces/Child-Namespaces/Namespaces/Namespace.cs
@@ -0,0 +1,14 @@
+namespace ParentNamespace
+{
+    namespace ChildNamespace
+    {
+        public class Class1
+        {
+            public void Method1()
+            {
+                // Implementation of Method1
+                System.Console.WriteLine("Output - ParentNamespace.ChildNamespace.Class1.Method1");
+            }
+        }
+    }
+}
---
+++ Child-Namespaces/Child-Namespaces/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ChildNamespaceExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            ParentNamespace.ChildNamespace.Class1 class1 = new ParentNamespace.ChildNamespace.Class1();
+            class1.Method1();
+        }
+    }
+}
