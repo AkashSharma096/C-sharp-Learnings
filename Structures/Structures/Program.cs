b564593f1479c577601c005bd4f9abb13ba8f693
AkashSharma023 <127305987+AkashSharma023@users.noreply.github.com>
2025-08-29 15:17:28 +0530

    Adding Structure

---
+++ Structures/Structures/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace StructureExample
+{
+    struct Student
+    {
+        public int Id;
+        public string Name;
+        public string marks;
+    }
+
+    class Program
+    {
+        public static void Main()
+        {
+            Student student1;
+
+            student1.Id = 1;
+            student1.Name = "John Doe";
+            student1.marks = "A";
+
+            System.Console.WriteLine($"ID: {student1.Id}, Name: {student1.Name}, Marks: {student1.marks}");
+        }
+    }
+}
