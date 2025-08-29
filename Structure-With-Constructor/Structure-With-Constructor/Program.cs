bfd1fc4637942f872ec8fe504e94e167dfc7a2e4
AkashSharma023 <127305987+AkashSharma023@users.noreply.github.com>
2025-08-29 15:27:48 +0530

    Adding Structure with Constructor

---
+++ Structure-With-Constructor/Structure-With-Constructor/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Structure_With_Constructor
+{
+    struct Student
+    {
+        public int Id;
+        public string Name;
+        public int Marks;
+        public string Grade;
+
+        public Student(int id, string name, int marks)
+        {
+            Id = id;
+            Name = name;
+            Marks = marks;
+            Grade = "None";
+        }
+
+        public void CalculateGrade()
+        {
+            if (Marks >= 90)
+                Grade = "A";
+            else if (Marks >= 80)
+                Grade = "B";
+            else if (Marks >= 70)
+                Grade = "C";
+            else if (Marks >= 60)
+                Grade = "D";
+            else
+                Grade = "F";
+        }
+
+    }
+
+    class Program
+    {
+        public static void Main()
+        {
+            Student student = new Student(1, "John Doe", 85);
+            student.CalculateGrade();
+            System.Console.WriteLine($"ID: {student.Id}, Name: {student.Name}, Marks: {student.Marks}, Grade: {student.Grade}");
+        }
+    }
+
+}
