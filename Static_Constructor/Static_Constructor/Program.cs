69ebcf9aa4c419604736efc96241a91635e3ae9b
AkashSharma023 <127305987+AkashSharma023@users.noreply.github.com>
2025-09-02 18:17:19 +0530

    Adding Static Constructor Example.

---
+++ Static_Constructor/Static_Constructor/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace StaticConstructor
+{
+    class Class1
+    {
+        public static int a, b;
+
+        static Class1()
+        {
+            a = 10;
+            b = 20;
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+
+            //Note: Static constructor is called only once here as Class1 is accessed first time inside Main method.
+
+            //Get Data from Static fields initialized by Static Constructor.
+            System.Console.WriteLine("Value of a: " + Class1.a);
+            System.Console.WriteLine("Value of b: " + Class1.b);
+        }
+    }
+}
