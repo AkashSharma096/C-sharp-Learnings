a875a8ead16dfcb61f9b0d0fbd7ac84a50001de1
AkashSharma023 <127305987+AkashSharma023@users.noreply.github.com>
2025-09-02 13:14:47 +0530

    Adding System.Object Class

---
+++ System.Object-Class/System.Object-Class/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace SystemObject_Class
+{
+    class Class1
+    {
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            // Create a reference variable of "System.Object" type.
+            System.Object obj;
+
+            // Create an instance of "Class1" and assign it to the "System.Object" reference variable.
+            obj = new Class1();
+
+            System.Console.WriteLine("Done");
+        }
+    }
+}
