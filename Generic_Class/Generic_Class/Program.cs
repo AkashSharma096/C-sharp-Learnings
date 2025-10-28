d4fba8e19fec82578b001a13834e5ee1886123c3
AkashSharma023 <127305987+AkashSharma023@users.noreply.github.com>
2025-10-28 13:06:04 +0530

    Adding Generic Class

---
+++ Generic_Class/Generic_Class/Program.cs
@@ -0,0 +1,18 @@
+﻿using generics;
+
+namespace generic_class
+{
+    class Program
+    {
+        static void Main()
+        {
+            User<string, int> user1 = new User<string, int> { userName = "Akash", userId = 1234 };
+            User<string, bool> user2 = new User<string, bool> { userName = "Ramesh", userId = false };
+
+
+            System.Console.WriteLine($"UserName:{user1.userName} and UserId : {user1.userId}");
+            System.Console.WriteLine($"UserName:{user2.userName} and UserId : {user2.userId}");
+
+        }
+    }
+}
---
+++ Generic_Class/Generic_Class/generics/User.cs
@@ -0,0 +1,10 @@
+namespace generics
+{
+    public class User<T1, T2>
+    {
+        public T1? userName;
+        public T2? userId;
+    }
+
+
+}
