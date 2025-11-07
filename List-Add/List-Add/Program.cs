3e6e34ca22f2705652e3691a59c1f92c2eed460f
AkashSharma023 <127305987+AkashSharma023@users.noreply.github.com>
2025-11-07 22:07:47 +0530

    Adding List Functions - Add/AddRange

---
+++ List-Add/List-Add/Program.cs
@@ -0,0 +1,46 @@
+﻿/**************************************************************************************************
+This is am example of a C# program that demonstrates how to add elements to a list.
+
+Syntax - ListName.Add(item);
+
+Parameters:
+  item: The object to be added to the end of the List<T>.
+  ListName: The name of the list to which the item will be added.
+  
+Example:
+    MyList.Add(42); // Adds the integer 42 to the end of MyList.  
+**************************************************************************************************/
+
+using System;
+using System.Collections.Generic;
+
+namespace List_Add
+{
+    public class Program
+    {
+        static void Main()
+        {
+            // Create a list of numbers
+
+            List<int> numbets = new List<int>(5) { 1, 2, 3, 4, 5 };
+
+            // Print the original list
+            Console.WriteLine("Original List:");
+            foreach (var number in numbets)
+            {
+                Console.WriteLine(number);
+            }
+
+            // Add a new number to the list
+            numbets.Add(6);
+            numbets.Add(7);
+
+            // Print the updated list
+            Console.WriteLine("\nUpdated List after adding elements:");
+            foreach (var number in numbets)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
---
+++ List-AddRange/List-AddRange/Program.cs
@@ -0,0 +1,44 @@
+﻿/**************************************************************************************************
+This is an example of a C# program that demonstrates the use of the List<T>.AddRange method to add multiple elements to a list.
+
+Syntax - MyList.AddRange(IEnumerable<T> OtherCollection)
+
+Parameters:
+    OtherCollection: An IEnumerable<T> collection containing the elements to add to the end of the List<T>.
+    MyList: The List<T> to which the elements will be added.
+
+Return Value - This method does not return a value.
+
+Example:
+    List<int> numbers = new List<int>() { 1, 2, 3 };
+    List<int> moreNumbers = new List<int>() { 4, 5, 6 };
+    numbers.AddRange(moreNumbers);
+**************************************************************************************************/
+using System;
+using System.Collections.Generic;
+
+namespace List_AddRange
+{
+    public class Pro
+    {
+        static void Main()
+        {
+            // create a list of integers
+            List<int> numbers = new List<int>(3) { 1, 2, 3 };
+
+            // create another list of integers to add
+            List<int> moreNumbers = new List<int>(3) { 4, 5, 6 };
+
+
+            // add the elements of moreNumbers to numbers
+            numbers.AddRange(moreNumbers);
+
+            // display the elements of numbers
+            Console.WriteLine("Elements of numbers list after AddRange:");
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
